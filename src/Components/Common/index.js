74024a54b1d75a94106b226f4fff6adac8a411dd
jordiibru <jordibrucarci@gmail.com>
2022-08-21 20:49:54 +0200

    reset

--- src/Components/Common/index.js
+++ src/Components/Common/index.js
@@ -1,9 +1,7 @@
 import Header from './header';
 import Navbar from './navbar';
-import Main from './main';
 
 export {
     Header,
     Navbar,
-    Main,
 }
--- src/Components/Common/main/Main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { Main } from '../../Common';
-import './Main.css';
-
-export default Main;
